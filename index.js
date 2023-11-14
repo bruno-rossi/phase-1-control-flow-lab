47267d45b62871da1c753966a032fb818c240d1b
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-11-14 13:34:18 -0500

    Submitting control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(mileage){
   // Write your code here!
+  if (mileage <= 400) {
+    return 'This one is on me!';
+  } else if (mileage <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (mileage > 2000 && mileage <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous': 
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
